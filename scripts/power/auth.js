1155d1ab76d38dcc59270c871897bffd2123bef3
Artem <a.khotsianovich@dev1team.net>
2015-06-29 15:04:37 +0300

    Refactor login to knockout instead of jquery

--- scripts/power/auth.js
+++ scripts/power/auth.js
@@ -1,61 +1,85 @@
-﻿function AuthModule(options) {
+﻿(function (globals) {
+    'use strict';
 
-    var defaultOptions = {
-        urlToLoadDialog: null,
-        urlToLogin: null,
+    function AuthModule(options) {
+
+        var defaultOptions = {
+            urlToLoadDialog: null
+        }
+
+        for (var option in defaultOptions)
+            this[option] = options && options[option] !== undefined ? options[option] : defaultOptions[option];
+
+        this.loadInlineLogin = function (response) {
+            var self = this;
+            if (!$('#inline-login-page').length) {
+                $.get(self.urlToLoadDialog, null,
+                    function (data) {
+                        $('body').append(data);
+                        openModal();
+                        setTimeout(function () {
+                            ko.applyBindings(loginViewModel, document.getElementById("login"))
+                            ko.applyBindings(forgotViewModel, document.getElementById("forgot"))
+                        }, 50);
+                    });
+            } else {
+                if (!$('#inline-login-page').hasClass('in')) {
+                    openModal();
+                }
+            }
+        }
+
+        var openModal = function () {
+            $('#inline-login-page').modal('show');
+        };
+
+        this.IsUnauthorizeResponse = function (xhr) {
+            return xhr.status == 403 || xhr.status == 401;
+        }
     }
 
-    for (var option in defaultOptions)
-        this[option] = options && options[option] !== undefined ? options[option] : defaultOptions[option];
-    //checkAjaxAuth
+    globals.AuthModule = AuthModule;
 
-    this.loadInlineLogin = function (response) {
+    function BaseAccountViewModel(model) {
         var self = this;
-        if (!$('#inline-login-page').length) {
-            $.get(self.urlToLoadDialog, null,
-                function (data) {
-                    $('body').append(data);
-                    openModal();
-                });
-        } else {
-            if (!$('#inline-login-page').hasClass('in')) {
-                openModal();
+
+        this.isAjax = false;
+        this.Message = ko.observable(model.Message);
+        this.MessageType = ko.observable(model.MessageType);
+        this.MessageCssClass = ko.pureComputed(function () {
+            if (self.MessageType() === 0)
+                return "alert alert-block alert-success"
+            else if (self.MessageType() === 1)
+                return "alert alert-block alert-danger"
+        });
+
+        this.Submit = function (vm, form) {
+            if (!vm) return false;
+            vm.errors.showAllMessages();
+            if (self.isAjax) {
+                if (vm.isValid()) {
+                    $.ajax({
+                        type: "POST",
+                        url: $(form).attr('action'),
+                        data: $(form).serialize(),
+                        success: function (data) {
+                            if (data.obj) {
+                                self.Message(data.obj.Message);
+                                self.MessageType(data.obj.MessageType);
+                            }
+                            if (data.success && !(data.obj && data.obj.MessageType === 0/*success*/)) {
+                                $('#inline-login-page').modal('hide');
+                            }
+                        }
+                    });
+                }
+                return false;
+            } else {
+                return vm.isValid();
             }
         }
     }
 
-    var openModal = function () {
-        $('#inline-login-page').modal('show');
-        setTimeout(function () {
-            $('#login-form').unbind().submit(replaceLoginAction);
-        }, 50);
-    };
+    globals.BaseAccountViewModel = BaseAccountViewModel;
 
-    var replaceLoginAction = function () {
-        $('#login-form').validate();
-        if ($('#login-form').valid()) {
-            $.ajax({
-                type: "POST",
-                url: authModule.urlToLogin,
-                data: $("#login-form").serialize(),
-                success: function (data) {
-                    var newForm = $(data).find('#login-form');
-                    if (newForm.length == 1) {
-                        $('#login-form').replaceWith(newForm);
-                        setTimeout(function () {
-                            $.validator.unobtrusive.parse(newForm);
-                            $('#login-form').submit(replaceLoginAction);
-                        }, 50);
-                    } else {
-                        $('#inline-login-page').modal('hide');
-                    }
-                }
-            });
-        }
-        return false;
-    }
-
-    this.IsUnauthorizeResponse = function (xhr) {
-        return xhr.status == 403 || xhr.status == 401;
-    }
-}
+}(this));
