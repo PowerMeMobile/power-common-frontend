f7c9a05ca68f77f1a76af5a01605f469c635d736
Artem <a.khotsianovich@dev1team.net>
2015-07-02 16:16:49 +0300

    Remote validation for knockout

--- scripts/power/customValidators.js
+++ scripts/power/customValidators.js
@@ -61,5 +61,27 @@ if (typeof ko != 'undefined') {
         message: 'The field must equal {0}'
     };
 
+    ko.validation.rules['remote'] = {
+        async: true,
+        validator: function (val, parms, callback) {
+            var defaults = {
+                type: 'POST',
+                success: function (data) {
+                    callback({ isValid: data.success, message: data.message });
+                }
+            };
+
+            var options = $.extend(defaults, parms);
+            options.data = {};
+            parms.fields.forEach(function (i) {
+                options.data[i] = ko.unwrap(parms.obj[i]);
+
+            });
+
+            $.ajax(options);
+        },
+        message: 'Invalid value'
+    };
+
     ko.validation.registerExtenders();
 }
