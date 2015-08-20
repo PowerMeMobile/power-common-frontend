4ae21ecfd10f48b042c92ac692ed72f6a0cd693b
Artem <a.khotsianovich@dev1team.net>
2015-08-20 20:20:24 +0300

    Add base edit view with block & alert ability

---
+++ scripts/power/baseEditViewModel.js
@@ -0,0 +1,27 @@
+﻿(function (globals, App, jQuery) {
+    'use strict';
+
+    function BaseEditViewModel() {
+        this.BlockingStatus = ko.observable();
+        this.Alert = ko.observable();
+    }
+
+    function BlockingStatus(isUpdating, success) {
+        return {
+            isUpdating: isUpdating,
+            success: success
+        };
+    }
+
+    function AlertStatus(success, message) {
+        return {
+            success: success,
+            message: message
+        }
+    }
+
+    globals.BlockingStatus = BlockingStatus;
+    globals.AlertStatus = AlertStatus;
+    globals.BaseEditViewModel = BaseEditViewModel;
+
+}(this, App, jQuery));
