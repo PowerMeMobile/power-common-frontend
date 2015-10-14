9b4c2f13f568e387f0fac8910a387872f061a5e8
Artem <a.khotsianovich@dev1team.net>
2015-10-14 17:30:52 +0300

    Add internal logs pages

---
+++ scripts/pages/internal-logs/internal-logs-view-model.js
@@ -0,0 +1,61 @@
+﻿(function (App, ko, $) {
+    'use strict;'
+
+    function InternalLogsViewModel(model) {
+        var self = this;
+
+        App.vms.Base.ServerTableViewModel.call(this, model);
+
+        this.From = ko.observable(model.From).subscribeToAction(this.Search);
+        this.To = ko.observable(model.To).subscribeToAction(this.Search);
+        this.Message = ko.observable(model.Message).extend({ rateLimit: 700 }).subscribeToAction(this.Search);
+        this.Levels = ko.observableArray(model.Levels).subscribeToAction(this.Search);
+        this.Levels.options = ko.observableArray(model.AvailableLevels);
+        this.Loggers = ko.observableArray(model.Loggers).subscribeToAction(this.Search);
+        this.Servers = ko.observableArray(model.Servers).subscribeToAction(this.Search);
+        this.Admins = ko.observableArray(model.Admins).subscribeToAction(this.Search);
+
+        var router = App.routers.Administration.InternalLogs;
+
+        this.loadLoggers = function (query) {
+            App.ajax.Select2Data(router.Loggers(), query);
+        }
+
+        this.loadServers = function (query) {
+            App.ajax.Select2Data(router.Servers(), query);
+        }
+
+        this.loadAdmins = function (query) {
+            App.ajax.Select2Data(router.Admins(), query);
+        }
+
+        this.tableOptions = function () {
+            return App.TablesHelper.composeServerOptions({
+                ajax: {
+                    url: router.Data(),
+                },
+                order: [[0, 'desc']],
+                columns: [
+                    {
+                        data: 'Date', render: function (data, type, obj) {
+                            return App.TablesHelper.linkTo(
+                                router.Details(obj.Id),
+                                App.TablesHelper.timeDate(data)
+                            );
+                        }
+                    },
+                    { data: 'Server' },
+                    { data: 'ThreadId' },
+                    { data: 'LogLevel' },
+                    { data: 'Admin' },
+                    { data: 'Logger' },
+                    { data: 'Message' }
+                ]
+            }, self);
+        }
+    }
+
+
+    App.ns('vms.Administration.Logs').InternalLogsViewModel = InternalLogsViewModel;
+
+}(App, ko, jQuery));
---
+++ scripts/pages/internal-logs/router.js
@@ -0,0 +1,17 @@
+﻿(function (App, ko, $) {
+    'use strict;'
+
+    function InternalLogsRouter(url) {
+        var baseUrl = url + 'Logs/';
+
+        this.Data = function () { return baseUrl + 'GetData'; }
+        this.Details = function (id) { return baseUrl + 'Details/' + id; }
+        this.Loggers = function () { return baseUrl + 'GetLoggers'; }
+        this.Servers = function () { return baseUrl + 'GetServers'; }
+        this.Admins = function () { return baseUrl + 'GetAdmins'; }
+    }
+
+
+    App.ns('routers.Administration').InternalLogs = new InternalLogsRouter(App.routers.baseUrl);
+
+}(App, ko, jQuery));
