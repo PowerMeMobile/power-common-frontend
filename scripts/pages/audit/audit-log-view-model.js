c536316132749e2b4698627dc99a3da21e347df5
Artem <a.khotsianovich@dev1team.net>
2015-10-14 16:31:00 +0300

    Add audit pages scripts

---
+++ scripts/pages/audit/audit-log-view-model.js
@@ -0,0 +1,59 @@
+﻿(function (App, ko, $) {
+    'use strict;'
+
+    function AuditLogViewModel(model) {
+        var self = this;
+
+        App.vms.Base.ServerTableViewModel.call(this, model);
+
+        this.From = ko.observable(model.From).subscribeToAction(this.Search);
+        this.To = ko.observable(model.To).subscribeToAction(this.Search);
+        this.Actions = ko.observableArray(model.Actions).subscribeToAction(this.Search);
+        this.Types = ko.observableArray(model.Types).subscribeToAction(this.Search);
+        this.Properties = ko.observableArray(model.Properties).subscribeToAction(this.Search);
+        this.Admins = ko.observableArray(model.Admins).subscribeToAction(this.Search);
+        this.Text = ko.observable(model.Text).extend({ rateLimit: 700 }).subscribeToAction(this.Search);
+
+        var router = App.routers.Administration.AuditLog
+
+        this.loadTypes = function (query) {
+            App.ajax.Select2Data(router.ItemTypes(), query);
+        }
+
+        this.loadProperties = function (query) {
+            App.ajax.Select2Data(router.Properties(), query);
+        }
+
+        this.loadAdmins = function (query) {
+            App.ajax.Select2Data(router.Admins(), query);
+        }
+
+        this.tableOptions = function () {
+            return App.TablesHelper.composeServerOptions({
+                ajax: {
+                    url: router.Data(),
+                },
+                order: [[0, 'desc']],
+                columns: [
+                    {
+                        data: 'Date', render: App.TablesHelper.timeDate
+                    },
+                    { data: 'Action' },
+                    {
+                        data: 'ItemType', render: function (data, type, obj) {
+                            return App.TablesHelper.linkTo(obj.Url, data);
+                        }
+                    },
+                    { data: 'Property' },
+                    { data: 'Admin', name: 'Admin.Name' },
+                    { data: 'OldValue' },
+                    { data: 'NewValue' },
+                    { data: 'DescriptionPart', name: 'Description' }
+                ]
+            }, self);
+        }
+    }
+
+    App.ns('vms.Administration.Logs').AuditLogViewModel = AuditLogViewModel;
+
+}(App, ko, jQuery));
---
+++ scripts/pages/audit/router.js
@@ -0,0 +1,16 @@
+﻿(function (App, ko, $) {
+    'use strict;'
+
+    function AuditLogRouter(url) {
+        var baseUrl = url + 'Audit/';
+
+        this.Data = function () { return baseUrl + 'GetData'; }
+        this.ItemTypes = function () { return baseUrl + 'GetTypes'; }
+        this.Properties = function () { return baseUrl + 'GetProperties'; }
+        this.Admins = function () { return baseUrl + 'GetAdmins'; }
+    }
+
+
+    App.ns('routers.Administration').AuditLog = new AuditLogRouter(App.routers.baseUrl);
+
+}(App, ko, jQuery));
