d66dc0b6bdedaaa09130fa76b6eeed071d2529e4
Artem <a.khotsianovich@dev1team.net>
2015-10-19 10:22:09 +0300

    Add change password viewModel

---
+++ scripts/pages/admins/change-password-view-model.js
@@ -0,0 +1,26 @@
+﻿(function (App, ko, $) {
+    'use strict;'
+
+    function ChangePasswordViewModel(model, router) {
+        var self = this;
+
+        App.vms.Base.BaseEditViewModel.call(this);
+
+        this.adminId = model.adminId;
+        this.oldPassword = ko.observable(model.oldPassword).extend({ required: true });
+        this.newPassword = ko.observable(model.newPassword).extend({ required: true });
+
+        App.vms.Base.BaseValidatableViewModel.call(this);
+
+        this.save = function () {
+            App.ajax.Save(router.changePassword(), self, function () {
+                self.oldPassword(null);
+                self.newPassword(null);
+                self.errors.showAllMessages(false);
+            });
+        }
+    }
+
+    App.ns('vms.Admins').ChangePasswordPage = ChangePasswordViewModel;
+
+}(App, ko, jQuery));
