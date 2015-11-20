3e8e33ecf84584d19000da63c1d74549fb75b818
Vasili Kauryha <vkovrigo@gmail.com>
2015-11-20 16:02:12 +0300

    Add knockout extender method for escape/unescape value

---
+++ scripts/power/extenders.js
@@ -0,0 +1,31 @@
+﻿(function(app, ko, undefined) {
+    'use strict';
+
+    if (ko === undefined) {
+        throw ('Knockout is required, please ensure it is loaded before loading this extenders.');
+    }
+
+    ko.extenders.escaped = function(target, options) {
+        var options = options || {},
+            onlyif = options.onlyif;
+
+        var result = ko.pureComputed({
+            read: function() {
+                return app.utils.unescape(target());
+            },
+            write: function(newValue) {
+                var currentValue = target(),
+                    escapedValue = app.utils.escape(app.utils.unescape(newValue));
+
+                if (currentValue !== escapedValue) {
+                    target(escapedValue);
+                }
+            }
+        });
+
+        result(target());
+
+        //return the new computed observable
+        return result;
+    };
+}(App, ko));
