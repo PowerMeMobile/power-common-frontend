97e66851bf5372ae167cfccd3b03b0dbaca76690
Vasili Kauryha <vkovrigo@gmail.com>
2015-11-25 14:18:56 +0300

    Add utils for strip html and ellipsis text

---
+++ scripts/utils/strip-html.js
@@ -0,0 +1,23 @@
+﻿(function(app, document, undefined) {
+    'use strict';
+
+    /**
+     * Extract pure text from HTML.
+     * @param {string} html - The content with HTML tags;
+     * @returns {string} - The clean text without HTML tags.
+     */
+    var striphtml = function(html) {
+        var text = '';
+
+        if (html !== '') {
+            var div = document.createElement('div');
+
+            div.innerHTML = html;
+            text = div.textContent || div.innerText;
+        }
+
+        return text;
+    };
+
+    app.ns('utils').stripHtml = striphtml;
+}(App, document));
---
+++ scripts/utils/text-ellipsis.js
@@ -0,0 +1,15 @@
+﻿(function(app, undefined) {
+    'use strict';
+
+    /**
+     * Cut text to defined length and adds ellipsis in the end.
+     * @param {string} text - The text for cut;
+     * @param {number} length - The max text length after cutting;
+     * @returns {type} - The cut text with ellipsis;
+     */
+    var ellipsis = function(text, length) {
+        return text.length > length ? text.substring(0, length) + '...' : text;
+    };
+
+    app.ns('utils').ellipsis = ellipsis;
+}(App));
