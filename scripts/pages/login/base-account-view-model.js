a0fa33d34e8358d47f2e2ec80fc273f2dff990bf
arthot <a.khotsianovich@dev1team.net>
2016-01-19 17:58:02 +0300

    Refactor base-account-view-model.js to lowerCase

--- scripts/pages/login/base-account-view-model.js
+++ scripts/pages/login/base-account-view-model.js
@@ -1,36 +1,35 @@
-﻿(function (App, ko) {
+﻿(function (App, ko, $) {
     'use strict';
 
     function BaseAccountViewModel(model) {
         var self = this;
 
         this.isAjax = false;
-        this.Message = ko.observable(model.Message).extend({ escaped: true });
-        this.MessageType = ko.observable(model.MessageType);
-        this.MessageCssClass = ko.pureComputed(function () {
-            if (self.MessageType() === 0 || self.MessageType() === "success")
+        this.message = ko.observable(model.message).extend({ escaped: true });
+        this.messageType = ko.observable(model.messageType);
+        this.messageCssClass = ko.pureComputed(function () {
+            if (self.messageType() === 0 || self.messageType() === "success")
                 return "alert alert-block alert-success"
-            else if (self.MessageType() === 1 || self.MessageType() === "danger")
+            else if (self.messageType() === 1 || self.messageType() === "danger")
                 return "alert alert-block alert-danger"
         });
 
-        this.Submit = function (vm, form) {
-            if (!vm) return false;
-            vm.errors.showAllMessages();
+        this.submit = function (form) {
+            self.errors.showAllMessages();
             if (self.isAjax) {
-                if (vm.isValid()) {
-                    jQuery.ajax({
+                if (self.isValid()) {
+                    $.ajax({
                         type: "POST",
-                        url: jQuery(form).attr('action'),
-                        data: jQuery(form).serialize(),
+                        url: $(form).attr('action'),
+                        data: $(form).serialize(),
                         success: function (data) {
                             if (data.obj) {
-                                self.Message(data.obj.Message);
-                                self.MessageType(data.obj.MessageType);
+                                self.message(data.obj.message);
+                                self.messageType(data.obj.messageType);
                             }
-                            if (data.success && !(data.obj && data.obj.MessageType === 0/*success*/)) {
-                                jQuery('#inline-login-page').modal('hide');
-                                if (data.obj.Admin && App.auth.User.Id != data.obj.Admin.Id) {
+                            if (data.success && !(data.obj && data.obj.message && data.obj.messageType === 'success')) {
+                                $('#inline-login-page').modal('hide');
+                                if (data.obj.admin && App.auth.User.Id != data.obj.admin.id) {
                                     document.location.reload(true);
                                 }
                             }
@@ -39,7 +38,7 @@
                 }
                 return false;
             } else {
-                return vm.isValid();
+                return self.isValid();
             }
         }
     }
@@ -47,24 +46,24 @@
     function BaseLoginViewModel(model) {
         var self = this;
 
-        this.Password = ko.observable(model.Password).extend({ required: true });
-        this.RememberMe = ko.observable(model.RememberMe);
+        this.password = ko.observable(model.password).extend({ required: true });
+        this.rememberMe = ko.observable(model.rememberMe);
 
         this.checkCaps = function (item, event) {
             var s = String.fromCharCode(event.which);
             var show = s.toUpperCase() === s && s.toLowerCase() !== s && !event.shiftKey;
-            self.ShowCapsWarning(show);
+            self.showCapsWarning(show);
             return true;
         }
 
         this.checkPassFocus = function (item, event) {
-            self.ShowCapsWarning(false);
+            self.showCapsWarning(false);
         }
 
-        this.ShowCapsWarning = ko.observable(false);
+        this.showCapsWarning = ko.observable(false);
     }
 
     App.ns('vms.login').BaseAccount = BaseAccountViewModel;
     App.ns('vms.login').BaseLogin = BaseLoginViewModel;
 
-}(App, ko));
+}(App, ko, jQuery));
