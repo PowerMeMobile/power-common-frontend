1fcf32502f1d6625fc650ee80eb53583b0b11f66
arthot <a.khotsianovich@dev1team.net>
2016-01-20 12:06:39 +0300

    Separate power/notifications.js to modules/notifications-module.js & pages/base/layout-notifications-view-model.js. Rename notificationsModule to App.notifications

---
+++ scripts/modules/notifications-module.js
@@ -0,0 +1,112 @@
+﻿(function (App, ko) {
+    'use strict';
+
+    function NotificationsModule() {
+
+        var self = this;
+
+        var defaultOptions = {
+            invalidRequestMaxCount: 5,
+            invalidRequestSleepsMaxCount: 3,
+            defaultTimeout: 3000,
+            defaultLongTimeout: 15000,
+            urlToWait: null,
+            urlToLoad: null,
+            urlToRead: null
+        }
+
+        var invalidRequestCount = 0,
+            invalidRequestSleepsCount = 0;
+
+        this.init = function (options) {
+            for (var option in defaultOptions)
+                self[option] = options && options[option] !== undefined ? options[option] : defaultOptions[option];
+        }
+
+        this.addNotification = function (notification) {
+            notificationsViewModel.notifications.unshift(notification);
+        }
+
+        this.connect = function () {
+            $.ajax({
+                url: self.urlToWait,
+                cache: false,
+                global: false,
+                success: function (data, s) {
+                    if (data) {
+                        notificationsViewModel.now(new Date());
+                        notificationsViewModel.notifications.unshift(new NotificationViewModel(data));
+                    }
+
+                    invalidRequestCount = 0;
+                    invalidRequestSleepsCount = 0;
+
+                    self.connect();
+                },
+                error: function (jqXHR, textStatus) {
+                    if (!App.auth.isUnauthorizeResponse(jqXHR)) {
+                        if (invalidRequestCount < self.invalidRequestMaxCount) {
+                            invalidRequestCount++;
+                            self.connect();
+                        } else {
+                            if (invalidRequestSleepsCount < self.invalidRequestSleepsMaxCount) {
+                                invalidRequestSleepsCount++;
+                                invalidRequestCount = 0;
+                                setTimeout(self.connect, self.defaultTimeout);
+                            } else {
+                                setTimeout(self.connect, self.defaultLongTimeout);
+                            }
+                        }
+                    } else {
+                        setTimeout(self.connect, self.defaultLongTimeout * 2);
+                    }
+                }
+            });
+        }
+
+        this.loadLastNotifications = function () {
+            $.ajax({
+                url: self.urlToLoad,
+                cache: false,
+                success: function (data, s) {
+                    if (data) {
+                        ko.utils.arrayPushAll(notificationsViewModel.notifications, data.map(function (el) {
+                            return new NotificationViewModel(el);
+                        }));
+                    }
+                },
+                error: function (jqXHR, textStatus) {
+                    notificationsViewModel.errorMessage("Error loading data from server");
+                },
+                complete: function () {
+                    notificationsViewModel.isLoading(false);
+                }
+            });
+        }
+
+        this.reload = function () {
+            $.ajax({
+                url: self.urlToLoad,
+                cache: false,
+                success: function (data, s) {
+                    if (data) {
+                        notificationsViewModel.notifications(data.map(function (el) {
+                            return new NotificationViewModel(el);
+                        }));
+                    }
+                },
+                error: function (jqXHR, textStatus) {
+                    notificationsViewModel.errorMessage("Error loading data from server");
+                },
+                complete: function () {
+                    notificationsViewModel.isLoading(false);
+                }
+            });
+        }
+    }
+
+
+
+    App.notifications = new NotificationsModule();
+
+}(App, ko));
---
+++ scripts/pages/base/layout-notifications-view-model.js
@@ -0,0 +1,49 @@
+﻿function NotificationViewModel(model) {
+    var self = this;
+
+    this.Id = model.Id;
+    this.AdminId = model.AdminId;
+    this.Type = model.Type;
+    this.Message = ko.observable(model.Message).extend({ escaped: true });
+    this.CallbackUrl = ko.observable(model.CallbackUrl).extend({ escaped: true });
+    this.Date = model.Date;
+    this.dateText = ko.computed(function () {
+        return moment(this.Date).from(notificationsViewModel.now());
+    }, this);
+    this.IsReaded = ko.observable(model.IsReaded);
+
+    this.callBack = function () {
+        if (self.CallbackUrl()) {
+            document.location.href = self.CallbackUrl();
+        }
+        $.ajax({
+            url: App.notifications.urlToRead,
+            method: "POST",
+            data: { messageId: self.Id },
+            success: function () {
+                self.IsReaded(true);
+                notificationsViewModel.notifications.remove(function (el) { return el.Id == self.Id });
+            }
+        });
+    }
+}
+
+function NotificationsViewModel() {
+    var self = this;
+
+    this.notifications = ko.observableArray().publishOn('changed.notifications');
+    this.lastActiveNotifications = ko.pureComputed(function () {
+        return self.notifications().slice(0, App.backend.Notifications ? App.backend.Notifications.ActiveNotificationsCount : 7);
+    });
+
+    this.totalNotificationsCount = ko.pureComputed(function () {
+        return self.notifications().length;
+    });
+    this.isLoading = ko.observable(true);
+    this.errorMessage = ko.observable();
+    this.now = ko.observable(new Date());
+
+    setInterval(function () { self.now(new Date()); }, 60 * 1000);
+}
+
+var notificationsViewModel = new NotificationsViewModel();
